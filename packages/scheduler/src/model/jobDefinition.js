244bd669df0e576ac79cdb47452a34dc419ea6bd
doojin <dmitry.papka@gmail.com>
2020-04-27 23:04:43 -0500

    refactoring scheduler

--- packages/scheduler/src/model/jobDefinition.js
+++
@@ -1,4 +0,0 @@
-const mongo = require('@polyphorm/crawler-mongo')
-const jobDefinitionSchema = require('../schema/jobDefinition')
-
-module.exports = mongo.getInstance().model('JobDefinition', jobDefinitionSchema)
--- packages/scheduler/src/scheduler.js
+++ packages/scheduler/src/scheduler.js
@@ -1,14 +1,5 @@
-const Job = require('./model/jobDefinition')
-
 module.exports = {
-  async getSchedule () {
-    const jobs = await Job.find({})
-    return jobs.map(job => job.toObject())
-  },
+  async schedule (configurationDir) {
 
-  async setSchedule (jobs) {
-    await Promise.all(jobs
-      .map(job => new Job(job))
-      .map(job => job.save()))
   }
 }
