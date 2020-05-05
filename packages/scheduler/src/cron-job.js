e798e2ff9a1fa072960e73824842acc24518be8c
doojin <dmitry.papka@gmail.com>
2020-05-05 14:23:05 -0500

    cron job now persists worker slots

--- packages/scheduler/src/cron-job.js
+++ packages/scheduler/src/cron-job.js
@@ -1,12 +1,22 @@
+const { models: { WorkerSlot } } = require('@polyphorm/crawler-domain')
+
 module.exports = class CronJob {
   constructor (job) {
     this.name = job.definition.name
     this.cronExpression = job.definition.cronExpression
     this.initialResources = job.definition.initialResources
     this.scripts = job.scripts
+    this.concrrency = job.definition.concurrency
   }
 
-  execute () {
+  async execute () {
+    const jobName = this.name
+    await WorkerSlot.deleteMany({ jobName })
+
+    for (let i = 0; i < this.concrrency; i++) {
+      await new WorkerSlot({ jobName }).save()
+    }
+
     console.log(`Job name: ${this.name}`)
     console.log(`Cron expression: ${this.cronExpression}`)
     console.log(`Scripts: ${this.scripts.length}`)
