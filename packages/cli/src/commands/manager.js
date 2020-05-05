b06667423414b6341b2a6e90c6e7915a54d17782
doojin <dmitry.papka@gmail.com>
2020-05-04 20:23:29 -0500

    adding new option for jobs configuration directory

--- packages/cli/src/commands/manager.js
+++ packages/cli/src/commands/manager.js
@@ -5,13 +5,16 @@ module.exports = {
     program.command('manager')
       .description('starts manager instance')
       .requiredOption('-c, --connection <connection>', 'database connection url')
+      .requiredOption('-d, --jobs-configuration-dir <jobsConfigurationDir>', 'directory with jobs configuration')
       .action(options => {
         const {
-          connection
+          connection,
+          jobsConfigurationDir
         } = options
 
         manager.run({
-          connection
+          connection,
+          jobsConfigurationDir
         })
       })
   }
