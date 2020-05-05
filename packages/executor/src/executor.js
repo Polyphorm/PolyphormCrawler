9a3ea26cadf72f3e8d7d082ef647341f195a904e
doojin <dmitry.papka@gmail.com>
2020-05-04 19:10:58 -0500

    implementing executor

--- packages/executor/src/executor.js
+++ packages/executor/src/executor.js
@@ -1,1 +1,9 @@
-module.exports = {}
+const fetcher = require('@polyphorm/crawler-fetcher')
+const extractor = require('@polyphorm/crawler-extractor')
+
+module.exports = {
+  async execute (resource, scripts) {
+    const page = await fetcher.fetch(resource.res, resource.fetchConfiguration)
+    return await extractor.extract(page, scripts, resource.extractorConfiguration)
+  }
+}
